774d4b2df20b50320fafec1720cee12391a7f397
Hitanbot <matthewjamesworkmail@gmail.com>
2020-06-24 14:18:19 +0100

    components done

---
+++ client/src/components/Booking.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="booking">
+    <h2>{{booking.name}}</h2>
+    <p>E-mail:{{booking.email}}<p>
+    <p>Checked in status:{{booking.checkedIn}}<p>
+    <button type="button" v-on:click="deleteBooking">Cancel Booking</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:booking,
+  props:['booking'],
+  methods:{
+    deleteBooking(){
+      BookingService.deleteBooking(this.booking._id)
+      .then(() => eventBus.$emit('booking-deleted',this.booking._id))
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/BookingsGrid.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+	<div id="bookingsGrid">
+		<booking v-for="booking in bookings" :booking="booking" />
+	</div>
+</template>
+
+<script>
+import Booking from './Booking';
+import { eventBus } from '../main.js';
+import BookingService from '../services/BookingService.js'
+
+export default {
+	name: 'bookings-grid',
+	components: {
+		'booking': Booking
+	},
+	props: ['bookings']
+}
+</script>
